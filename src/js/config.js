be8141874c8eef39cb1b1dad5792f742396ea3cd
Dea314 <fabrisdanijela@gmail.com>
2022-11-25 15:00:42 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 9;
+export const RES_PER_PAGE = 10;
+export const KEY = "6f4cdcf2-7ae8-43fc-b540-fd5b7538a964";
+export const MODAL_CLOSE_SEC = 2.5;
